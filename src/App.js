b05f32740a433d8f191b6f7d4a9306ab190bf213
Abrar Khan <abrarkhan12911@gmail.com>
2024-05-15 16:32:35 +0530

    modified app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Deployed by Abrar Khan
+        Learn React
         </a>
       </header>
     </div>
