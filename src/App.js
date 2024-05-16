ae32ad3ca009f5b01fc81ea30136697323e6f9ff
Abrar Khan <abrarkhan12911@gmail.com>
2024-05-16 17:20:53 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-        Learn React
+        This is deployed by using Git actions
         </a>
       </header>
     </div>
